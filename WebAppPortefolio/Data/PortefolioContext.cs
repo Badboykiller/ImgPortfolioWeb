3bcca461f5cb8ea0541cf93319ea7c5bbb4192a2
André Silva <andre.f.m.silva@hotmail.com>
2018-07-03 12:30:10 +0100

    Fisrt part DbContext

---
+++ WebAppPortefolio/Data/PortefolioContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppPortefolio.Models;
+
+namespace WebAppPortefolio.Data
+{
+    public class PortefolioContext : DbContext
+    {
+        public PortefolioContext(DbContextOptions<PortefolioContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Utilizador> Utilizadores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuiler)
+        {
+            modelBuiler.Entity<Utilizador>().ToTable("Utilizador");
+        }
+    }
+}
---
+++ WebAppPortefolio/Models/Utilizador.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppPortefolio.Models
+{
+    public class Utilizador
+    {
+        public Utilizador()
+        {
+            IsActive = true;
+            DateCreated = DateTime.Now;
+        }
+
+        public int ID { get; set; }
+        public String Nome { get; set; }
+        public String Username { get; set; }
+        public String Email { get; set; }
+        public String PasswordH { get; set; }
+
+        public bool IsActive { get; set; }
+        public DateTime DateCreated {get; set;}
+    }
+}
